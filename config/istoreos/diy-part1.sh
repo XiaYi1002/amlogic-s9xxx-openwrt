11f8da97c68415798ceb40badd5ef7db18ca35eb
Onlyoooooo <674824021@qq.com>
2025-07-12 21:07:16 +0800

    Update diy-part1.sh

--- config/istoreos/diy-part1.sh
+++ config/istoreos/diy-part1.sh
@@ -3,17 +3,36 @@
 # https://github.com/ophub/amlogic-s9xxx-openwrt
 # Description: Automatically Build OpenWrt
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
+# Source code repository: https://github.com/istoreos/istoreos / Branch: master
 #========================================================================================================================
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# 部分仓库是多余的
+# 添加 nikki 仓库
 sed -i '1i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
-
-# Add luci-app-passwall2
+# 添加 third_party 仓库
+sed -i '1i src-git third_party https://github.com/linkease/istore-packages.git;main' feeds.conf.default
+# 添加 diskman 仓库
+sed -i '1i src-git diskman https://github.com/jjm2473/luci-app-diskman.git;dev' feeds.conf.default
+# 添加 oaf 仓库
+sed -i '1i src-git oaf https://github.com/jjm2473/OpenAppFilter.git;dev6' feeds.conf.default
+# 添加 linkease_nas 仓库
+sed -i '1i src-git linkease_nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+# 添加 linkease_nas_luci 仓库
+sed -i '1i src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+# 添加 jjm2473_apps 仓库
+sed -i '1i src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git;main' feeds.conf.default
+# 添加 luci-app-passwall2
 rm -rf package/luci-app-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
-
+# 添加 晶晨宝盒
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+# 添加 lucky
+rm -rf package/lucky
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+ 
+ 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
