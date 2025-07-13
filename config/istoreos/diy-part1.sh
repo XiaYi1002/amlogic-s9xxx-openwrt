b68061e0eb3d682ccab6c79baaee5be3b65ab803
XiaYi1002 <411194236@qq.com>
2025-07-13 21:35:15 +0800

    Update diy-part1.sh

--- config/istoreos/diy-part1.sh
+++ config/istoreos/diy-part1.sh
@@ -8,20 +8,13 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# 添加 nikki 仓库
-sed -i '1i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
-# 添加 third_party 仓库
-sed -i '1i src-git third_party https://github.com/linkease/istore-packages.git;main' feeds.conf.default
-# 添加 diskman 仓库
-sed -i '1i src-git diskman https://github.com/jjm2473/luci-app-diskman.git;dev' feeds.conf.default
-# 添加 oaf 仓库
-sed -i '1i src-git oaf https://github.com/jjm2473/OpenAppFilter.git;dev6' feeds.conf.default
-# 添加 linkease_nas 仓库
-sed -i '1i src-git linkease_nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-# 添加 linkease_nas_luci 仓库
-sed -i '1i src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-# 添加 jjm2473_apps 仓库
-sed -i '1i src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git;main' feeds.conf.default
+sed -i '$a src-git third_party https://github.com/linkease/istore-packages.git;main' feeds.conf.default
+sed -i '$a src-git diskman https://github.com/jjm2473/luci-app-diskman.git;dev' feeds.conf.default
+sed -i '$a src-git oaf https://github.com/jjm2473/OpenAppFilter.git;dev6' feeds.conf.default
+sed -i '$a src-git linkease_nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+sed -i '$a src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+sed -i '$a src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git;main' feeds.conf.default
+
 # 添加 晶晨宝盒
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
